bb7ff56a4fce7077add7d1cce163d78ff126a298
Bayu Fanani <fanani707@gmail.com>
2019-11-24 11:54:16 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//@ts-check
+/** @type HTMLElement */
+let chats = document.getElementById('chats');
+let txtPesan = /** @type HTMLInputElement */ (document.getElementById('txtPesan'));
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('btnKirim').addEventListener('click', function () {
+    mengirim({
+      pesan: txtPesan.value,
+      aksi: 'kirim',
+      penerima: activeWith
+    });
+  });
+});
+
+function mengirim(payload) {
+  let chat = document.createElement('div');
+  chat.className = 'chat right';
+  let yours = document.createElement('div');
+  yours.className = 'yours';
+  let yoursText = document.createTextNode(payload.pesan);
+  yours.appendChild(yoursText);
+  chat.appendChild(yours);
+  let shouldScroll = false;
+  let scrolled = chats.scrollTop + chats.clientHeight;
+  if (scrolled == chats.scrollHeight) {
+    shouldScroll = true;
+  }
+  chats.appendChild(chat);
+  if (shouldScroll) {
+    chats.scrollTo(0, chats.scrollHeight);
+  }
+  txtPesan.value = "";
+  connection.send(convertToPayload(payload));
+}
---
+++ websocket.js
@@ -0,0 +1,108 @@
+let connection = new WebSocket('ws://127.0.0.1:3000');
+let myUsername;
+let activeWith;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  // if user is running mozilla then use it's built-in WebSocket
+  window.WebSocket = window.WebSocket || window.MozWebSocket;
+  connection.onopen = function () {
+    // connection is opened and ready to use
+    tanyakanUsername();
+  };
+
+  connection.onerror = function (error) {
+    // an error occurred when sending/receiving data
+    alert('errro:' + error);
+  };
+
+  connection.onmessage = function (message) {
+    // try to decode json (I assume that each message
+    // from server is json)
+    console.log(message.data);
+    let jsonMessage = JSON.parse(message.data);
+    switch (jsonMessage.aksi) {
+      case 'refreshUserOnline':
+        refreshUserOnline(jsonMessage.usernames);
+        break;
+      case 'terima':
+        terimaPesan(jsonMessage.pesan, jsonMessage.pengirim);
+        break;
+      default:
+        console.log('no action needed!');
+
+        break;
+    }
+
+    /* try {
+      var json = JSON.parse(message.data);
+      console.log(json);
+    } catch (e) {
+      console.log('This doesn\'t look like a valid JSON: ',
+        message.data);
+      return;
+    } */
+    // handle incoming message
+  };
+});
+/**
+ * 
+ * @param {array} usernames 
+ */
+function refreshUserOnline(usernames) {
+  let el = document.getElementById('users-online');
+  while (el.firstChild) {
+    el.removeChild(el.firstChild);
+  }
+  usernames.forEach(function (username) {
+    if (myUsername != username) {
+      let elUser = document.createElement('div');
+      elUser.className = 'user';
+      elUser.onclick = function () {
+        let el = document.getElementById('chat-with');
+        el.innerHTML = username;
+        activeWith = username;
+      };
+      let elFotoThumbnail = document.createElement('div');
+      elFotoThumbnail.className = 'foto-thumbnail';
+      let h4 = document.createElement('h4');
+      let h4Text = document.createTextNode(username);
+      h4.appendChild(h4Text);
+      elUser.appendChild(elFotoThumbnail);
+      elUser.appendChild(h4);
+      el.appendChild(elUser);
+    }
+  });
+}
+
+function terimaPesan(pesan, dari) {
+  if (activeWith == dari) {
+    let el = document.getElementById('chats');
+    let chat = document.createElement('div');
+    chat.className = 'chat';
+    let reply = document.createElement('div');
+    reply.className = 'reply';
+    let replyText = document.createTextNode(pesan);
+    reply.appendChild(replyText);
+    chat.appendChild(reply);
+    el.appendChild(chat);
+  }
+}
+
+function tanyakanUsername() {
+  let username = prompt('Halo, masukkan nama anda:');
+  daftarkanUsername(connection, username);
+}
+
+function daftarkanUsername(con, username) {
+  let payload = convertToPayload({
+    username: username,
+    aksi: 'daftar'
+  });
+  con.send(payload);
+  myUsername = username;
+  document.getElementById('nama-profil').innerHTML = myUsername;
+}
+
+function convertToPayload(json) {
+  return JSON.stringify(json);
+}
